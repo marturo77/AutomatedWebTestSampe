b3ce9919a52ccc597a08a4b083a5c38b068e865d
Arturo Duarte <marturo77@hotmail.com>
2024-10-05 17:17:35 -0500

    Ajuste de hook y navegador

--- WebApp.Spec/StepDefinitions/CargaMasivaDeRegistrosStepDefinitions.cs
+++ WebApp.Spec/StepDefinitions/CargaMasivaDeRegistrosStepDefinitions.cs
@@ -33,7 +33,6 @@ namespace WebApp.Spec.StepDefinitions
             }
             catch (Exception ex)
             {
-                app.Close();
                 Assert.Fail($"La prueba falló debido a: {ex.Message}");
             }
         }
@@ -41,55 +40,46 @@ namespace WebApp.Spec.StepDefinitions
         [Given(@"que existe un archivo CSV con el listado de pólizas con todos los campos requeridos")]
         public void GivenQueExisteUnArchivoCSVConElListadoDePolizasConTodosLosCamposRequeridos()
         {
-            throw new PendingStepException();
         }
 
         [Given(@"que el usuario está en la página de carga masiva de pólizas")]
         public void GivenQueElUsuarioEstaEnLaPaginaDeCargaMasivaDePolizas()
         {
-            throw new PendingStepException();
         }
 
         [When(@"el usuario selecciona el archivo ""([^""]*)""")]
         public void WhenElUsuarioSeleccionaElArchivo(string p0)
         {
-            throw new PendingStepException();
         }
 
         [When(@"el usuario hace clic en el botón de ""([^""]*)""")]
         public void WhenElUsuarioHaceClicEnElBotonDe(string cargar)
         {
-            throw new PendingStepException();
         }
 
         [Then(@"el sistema debería procesar el archivo y poblar la base de datos de pólizas")]
         public void ThenElSistemaDeberiaProcesarElArchivoYPoblarLaBaseDeDatosDePolizas()
         {
-            throw new PendingStepException();
         }
 
         [Then(@"se muestra un mensaje ""([^""]*)""")]
         public void ThenSeMuestraUnMensaje(string p0)
         {
-            throw new PendingStepException();
         }
 
         [Given(@"que existe un archivo CSV con el listado de pólizas con campos faltantes")]
         public void GivenQueExisteUnArchivoCSVConElListadoDePolizasConCamposFaltantes()
         {
-            throw new PendingStepException();
         }
 
         [Then(@"el sistema debería mostrar un mensaje de error ""([^""]*)""")]
         public void ThenElSistemaDeberiaMostrarUnMensajeDeError(string p0)
         {
-            throw new PendingStepException();
         }
 
         [When(@"el usuario selecciona un archivo con formato diferente a CSV")]
         public void WhenElUsuarioSeleccionaUnArchivoConFormatoDiferenteACSV()
         {
-            throw new PendingStepException();
         }
     }
 }
